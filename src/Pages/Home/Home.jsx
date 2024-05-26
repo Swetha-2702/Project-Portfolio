fd8a4996991a1bf327ce8ad87d81f1d0da2f4b82
Swetha-2702 <swethaezhilmaran@gmail.com>
2024-05-26 21:50:42 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             <div className="content-box col-lg-7">
               <div className="type-box">
                 <h4>Hello , My Name is</h4>
-                <h1>Lithiksha.A.S</h1>
+                <h1>Lithiksha A S</h1>
                 <p className="text">
                   {text}
                   <Cursor
