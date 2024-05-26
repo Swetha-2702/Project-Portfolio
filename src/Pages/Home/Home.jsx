87c9202ed28d38b9cb6f9c062c62338834f4ecf9
Swetha-2702 <swethaezhilmaran@gmail.com>
2024-05-26 19:45:11 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { Fade, Zoom } from "react-awesome-reveal";
 import "./Home.css";
-import Profile from '../../assets/photo.jpg';
+import Profile from '../../assets/photo.jpeg';
 
 const Home = () => {
   const [text] = useTypewriter({
